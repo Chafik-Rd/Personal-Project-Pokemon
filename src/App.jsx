64679f8285d2c54b494a74b15005e0b9f02ac018
Chafik-Rd <78787036+Chafik-Rd@users.noreply.github.com>
2024-09-15 17:00:14 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CardPokemon from "./components/CardPokemon"
+import NavBar from "./components/NavBar"
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <CardPokemon/>
+    </>
+  )
+}
+
+export default App
